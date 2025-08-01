678dbe107d4e825e18a422d1af190af4c039c911
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-31 23:41:37 -0700

    updated subheading component

--- frontend/src/components/SubHeading.jsx
+++ frontend/src/components/SubHeading.jsx
@@ -1,5 +1,7 @@
-export function SubHeading({label}) {
-  return <div className="text-slate-500 text-md pt-1 px-4 pb-4">
-    {label}
-  </div>
+export function SubHeading({ label }) {
+  return (
+    <div className="text-slate-600 text-base pt-1 px-3 pb-4 max-w-prose tracking-wide leading-relaxed">
+      {label}
+    </div>
+  );
 }
