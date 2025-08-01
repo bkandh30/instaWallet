8bd7ecd2798359695f1d78240f8726c4572cb63e
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-31 17:16:29 -0700

    setup user mongoose schema

---
+++ backend/db.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const { minLength, maxLength } = require("zod");
+require('dotnev').config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Mongo Connected"));
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+};
