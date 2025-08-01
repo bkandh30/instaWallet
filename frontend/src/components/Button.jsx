71f5f201cc3c014bcdeaaa2e827f9f4373d77dd1
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-31 23:29:34 -0700

    updated button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,3 +1,8 @@
 export function Button({label, onClick}) {
-    return <button onClick={onClick} type="button" class="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">{label}</button>
+    return <button onClick={onClick} 
+            type="button" 
+            class="w-full text-white bg-black hover:bg-gray-800 
+                border border-gray-900 rounded-md inline-flex 
+                items-center justify-center py-2 px-5 text-center 
+                text-base font-medium transition">{label}</button>
 }
