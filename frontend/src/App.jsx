0f09df56d898ff8fb6438cb556f8d9533a539ad4
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-31 22:56:58 -0700

    added button component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
-import './App.css'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
