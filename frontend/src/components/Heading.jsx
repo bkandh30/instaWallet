c9075b9798f84162a9e07bc537ececa5de5a01f0
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-31 23:37:57 -0700

    updated heading component

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -1,5 +1,7 @@
-export function Heading({label}) {
-    return <div className="font-bold text-4xl pt-6">
-      {label}
+export function Heading({ label }) {
+  return (
+    <div className="font-extrabold text-4xl pt-6 text-gray-800 tracking-tight leading-snug relative inline-block">
+        {label}
     </div>
+  );
 }
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -9,12 +9,12 @@ export const Signin = () => {
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-        <Heading label={"Sign in"} />
+        <Heading label={"Sign In"} />
         <SubHeading label={"Enter your credentials to access your account"} />
         <InputBox placeholder="bhavya.kandhari.eng@gmail.com" label={"Email"} />
         <InputBox placeholder="HellYeah88" label={"Password"} />
         <div className="pt-4">
-          <Button label={"Sign in"} />
+          <Button label={"Sign In"} />
         </div>
         <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
       </div>
