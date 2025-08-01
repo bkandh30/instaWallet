e9c9fa68c11ce25eaa98b6bdc5ad8c98aaa1d95d
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-31 17:55:29 -0700

    added user router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const userRouter = require("./user");
 
 const router = express.Router();
 
-module.exports = router
+router.use("/user", userRouter)
+
+module.exports = router;
