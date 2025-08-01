fd8801cbf5b4a4d7642eaf402c45d3d2a06303b7
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-31 22:08:22 -0700

    added routing to react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,16 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
