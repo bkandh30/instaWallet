2dd666eda0d7ecae5520b355f0ea7b9e6965e76c
bkandh30 <kandhari.bhavya20@gmail.com>
2025-08-03 12:10:02 -0700

    created dashboard webpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
-// import { Dashboard } from "./pages/Dashboard";
+import { Dashboard } from "./pages/Dashboard";
 // import { SendMoney } from "./pages/SendMoney";
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
