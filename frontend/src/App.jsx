0ef4e0426fc08e198ac63912ec83bcd1e058c448
bkandh30 <kandhari.bhavya20@gmail.com>
2025-08-03 12:41:08 -0700

    added sendmoney page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import {
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
-// import { SendMoney } from "./pages/SendMoney";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
     </>
